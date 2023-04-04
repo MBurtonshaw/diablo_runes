e2ffc63a4f1dd3da6ca11442a2d8f193725596b7
Matt <mburtonshaw@gmail.com>
2023-04-04 17:12:45 -0400

    working w state

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,10 +1,34 @@
-import { React } from 'react';
+import { React, useEffect, useState } from 'react';
+import data from '../data.json';
 
-export default function Footer() {
+export default function Items() {
+    let item_info = data.items;
+    
+
+    let [ items, setItems ] = useState('loading items...');
+    function getData() {
+      setItems(item_info);
+    }
+
+    useEffect( () => { getData() }, [ setItems ] );
+   
+    if (items !== 'loading items...') {
+
       return(
-        <div id="items_container" className="">
-            <p>items</p>
+        <div>
+          {items.forEach(piece=> <p>{piece}</p>)}
         </div>
       );
+    } else {
+      return('loading items...');
+      
+      
+
+    }
+    
+
+
+
+
 }
 
--- src/components/Runes.js
+++ src/components/Runes.js
@@ -1,10 +1,41 @@
-import { React } from 'react';
+import { React, useEffect, useState } from 'react';
+import data from '../data.json';
 
 export default function Runes() {
+    let item_info = data.items;
+    
+
+    let [ items, setItems ] = useState('loading items...');
+    function getData() {
+      setItems(item_info);
+    }
+
+    useEffect( () => { getData() }, [ setItems ] );
+   
+    if (items !== 'loading items...') {
+      
+      const things_1 = items.item_1;
+      const things_2 = items.item_2;
       return(
-        <div id="runes_container" className="">
-            <p>runes</p>
-        </div>
+          <div>
+            <div>
+                {things_1.map((part, i) => <p key={i}>{part}</p>)}
+              </div>
+              <div>
+              {things_2.map((part, i) => <p key={i}>{part}</p>)}
+            </div>
+          </div>
+        
       );
+
+      
+      
+
+    }
+    
+
+
+
+
 }
 
