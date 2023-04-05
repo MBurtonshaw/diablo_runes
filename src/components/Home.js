bfa6163e4b7d1fe67e28f806a906c2e5b84f783c
Matt <mburtonshaw@gmail.com>
2023-04-05 14:39:35 -0400

    done minus maybe extra styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,10 @@
 import { React } from 'react';
 import Runes from './Runes';
-import Items from './Items';
 
 export default function Home() {
       return(
         <div id="home_container" className="container">
             <Runes />
-           
         </div>
       );
 }
--- src/components/Runes.js
+++ src/components/Runes.js
@@ -14,15 +14,47 @@ export default function Runes() {
    
     if (items !== 'loading items...') {
       
-      const things_1 = items[0];
-      const things_2 = items.item_2;
-      console.log(things_1.name);
-      console.log(things_1.runes[0])
+      const things_1 = items[0].name;
+      const one_runes = items[0].runes.map((rune, i) => <p key={i} className='card-text'>{rune}</p>);
+
+      const things_2 = items[1].name;
+      const two_runes = items[1].runes.map((rune, i) => <p key={i}>{rune}</p>);
+
+      const things_3 = items[2].name;
+      const three_runes = items[2].runes.map((rune, i) => <p key={i}>{rune}</p>);
+
+      const things_4 = items[3].name;
+      const four_runes = items[3].runes.map((rune, i) => <p key={i}>{rune}</p>);
+
       return(
-          <div>
-           
+        <div className='container bg-dark'>
+          <div className="card-group m-5 p-5">
+            <div className="card bg-light">
+              <div className="card-body">
+                <h5 className="card-title pb-5">{things_1}</h5>
+                {one_runes}
+              </div>
+            </div>
+            <div className="card bg-light">
+              <div className="card-body">
+                <h5 className="card-title pb-5">{things_2}</h5>
+                {two_runes}
+              </div>
+            </div>
+            <div className="card bg-light">
+              <div className="card-body bg-light">
+                <h5 className="card-title pb-5">{things_3}</h5>
+                {three_runes}
+              </div>
+            </div>
+            <div className="card bg-light">
+              <div className="card-body">
+                <h5 className="card-title pb-5">{things_4}</h5>
+                {four_runes}
+              </div>
+            </div>
           </div>
-        
+        </div>
       );
 
       
