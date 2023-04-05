d23be7cd8e129d25c3e10abf947ce2e58b2ac231
Matt <mburtonshaw@gmail.com>
2023-04-05 14:09:43 -0400

    json fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
       return(
         <div id="home_container" className="container">
             <Runes />
-            <Items />
+           
         </div>
       );
 }
--- src/components/Runes.js
+++ src/components/Runes.js
@@ -14,16 +14,13 @@ export default function Runes() {
    
     if (items !== 'loading items...') {
       
-      const things_1 = items.item_1;
+      const things_1 = items[0];
       const things_2 = items.item_2;
+      console.log(things_1.name);
+      console.log(things_1.runes[0])
       return(
           <div>
-            <div>
-                {things_1.map((part, i) => <p key={i}>{part}</p>)}
-              </div>
-              <div>
-              {things_2.map((part, i) => <p key={i}>{part}</p>)}
-            </div>
+           
           </div>
         
       );
